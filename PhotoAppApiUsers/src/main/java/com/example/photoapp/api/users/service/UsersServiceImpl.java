262c342c962656392ea0731ea511139385d382cf
= <=>
2021-10-16 15:49:55 +0530

    user sign up complete

--- PhotoAppApiUsers/src/main/java/com/example/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/example/photoapp/api/users/service/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.photoapp.api.users.data.UserEntity;
@@ -17,14 +18,17 @@ public class UsersServiceImpl implements UsersService {
 	@Autowired
 	UsersRepository usersRepository;
 
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		
 		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
-		userEntity.setEncryptedPassword("test");
 		
 		usersRepository.save(userEntity);
 		
