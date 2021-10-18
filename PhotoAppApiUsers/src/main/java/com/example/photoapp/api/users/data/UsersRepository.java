d292e7a84d1e875a81be34500bc49f981f9ce867
= <=>
2021-10-18 16:05:31 +0530

    user login complete with security

--- PhotoAppApiUsers/src/main/java/com/example/photoapp/api/users/data/UsersRepository.java
+++ PhotoAppApiUsers/src/main/java/com/example/photoapp/api/users/data/UsersRepository.java
@@ -3,5 +3,6 @@ package com.example.photoapp.api.users.data;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsersRepository extends CrudRepository<UserEntity, Long> {
-
+	
+	UserEntity findByEmail(String email);
 }
